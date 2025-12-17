e12c61bd494feb4724b4181363191d5dc3ac0939
Pratik <praktik@BZHXL34.lan.uow.edu.au>
2025-12-17 13:23:42 +1100

    README file modified

--- run_burger.sh
+++ run_burger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## running simulations for Burgers' equation ##
-# python src/python_scripts/burger/generate_burger-sample.py
+python src/python_scripts/burger/generate_burger-sample.py
 python src/python_scripts/burger/FNO-nonlocal-Burger.py
 python src/python_scripts/burger/ConvLSTM-Burger.py
 python src/python_scripts/burger/STDK-nonlocal-Burger.py
